7409ebbc1263baef6d6d63ec9cd7f5109c34c0fd
Yeşim Gürbüzer <ysmgurbuzerr@gmail.com>
2024-01-01 14:52:08 +0300

    search functionality added

--- GutenbergPresentation/Controllers/HomeController.cs
+++ GutenbergPresentation/Controllers/HomeController.cs
@@ -1,26 +1,33 @@
 using GutenbergPresentation.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 using System.Diagnostics;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace GutenbergPresentation.Controllers
 {
-  
+
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly HttpClient _httpClient;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, HttpClient httpClient)
         {
             _logger = logger;
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri("https://gutendex.com/");
         }
 
         public IActionResult Index()
         {
-            
-                return View();
-           
-            
+
+            return View();
+
+
         }
         [Authorize]
         public IActionResult Privacy()
@@ -33,5 +40,35 @@ namespace GutenbergPresentation.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
+        public async Task<IActionResult> SearchBooks(string search)
+        {
+            string encodedSearch = Uri.EscapeDataString(search);
+            string endpoint = $"/books?search={encodedSearch}";
+
+            HttpResponseMessage response = await _httpClient.GetAsync(endpoint);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string apiResponse = await response.Content.ReadAsStringAsync();
+
+                BookResultViewModel bookResult = JsonConvert.DeserializeObject<BookResultViewModel>(apiResponse);
+             
+                return View("SearchBooks", bookResult);
+            }
+            else
+            {
+                return View("Error");
+            }
+        }
+
+
+
+
+
+
+
     }
+
 }
